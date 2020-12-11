daa9c518cbf045f5e32ac131e2936e93ca765a9e
daiwenwen399 <944148423@qq.com>
2020-12-11 11:34:41 +0800

    feat:完成品牌管理静态组件搭建

--- src/views/product/trademark/list.vue
+++ src/views/product/trademark/list.vue
@@ -1,11 +1,57 @@
 <template>
-<div>
-  品牌管理
-</div>
+  <div>
+    <!-- 添加按钮 -->
+    <el-button type="primary" icon="el-icon-plus">添加</el-button>
+    <!-- 表格 -->
+    <el-table :data="tableData" border style="width: 100%; margin: 20px 0">
+      <el-table-column prop="id" label="序号" width="80" align="center">
+      </el-table-column>
+      <el-table-column prop="name" label="品牌名称"> </el-table-column>
+      <el-table-column label="品牌LOGO">
+        <template slot-scope="scope">
+          <img :src="scope.row.logo" alt="logo" class="trademark_img" />
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template>
+          <el-button type="warning" icon="el-icon-edit">修改</el-button>
+          <el-button type="danger" icon="el-icon-delete">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 分页器 -->
+    <el-pagination
+      layout="prev, pager, next,jumper,sizes,total"
+      :total="50"
+      class="trademark_pagination"
+    >
+    </el-pagination>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'TrademarkList',
-}
+  name: "TrademarkList",
+  data() {
+    return {
+      tableData: [
+        {
+          id: 1,
+          name: "支付宝",
+          logo:
+            "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3560973341,161838141&fm=26&gp=0.jpg",
+        },
+      ],
+    };
+  },
+};
 </script>
+
+<style lang="sass">
+.trademark_img
+  width: 100px
+.trademark_pagination
+  text-align: center
+.el-pagination__sizes
+  margin-left: 20px
+</style>
