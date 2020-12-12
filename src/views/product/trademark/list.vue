9ebeb9465047e1dc347afc91ac898f7d9fdb1031
daiwenwen399 <944148423@qq.com>
2020-12-12 14:43:29 +0800

    feat:完善优化品牌管理界面的功能

--- src/views/product/trademark/list.vue
+++ src/views/product/trademark/list.vue
@@ -1,14 +1,13 @@
 <template>
   <div>
     <!-- 添加按钮 -->
-    <el-button
-      type="primary"
-      icon="el-icon-plus"
-      @click="addDialogVisible = true"
-      >添加</el-button
-    >
+    <el-button type="primary" icon="el-icon-plus" @click="add">添加</el-button>
     <!-- 添加的弹出框 -->
-    <el-dialog title="添加品牌" :visible.sync="addDialogVisible" width="50%">
+    <el-dialog
+      :title="`${trademarkForm.id ? '修改' : '添加'}品牌`"
+      :visible.sync="visible"
+      width="50%"
+    >
       <span>
         <el-form
           :model="trademarkForm"
@@ -48,7 +47,7 @@
         </el-form>
       </span>
       <span slot="footer">
-        <el-button @click="addDialogVisible = false">取 消</el-button>
+        <el-button @click="visible = false">取 消</el-button>
         <el-button type="primary" @click="submitForm('trademarkForm')"
           >确 定</el-button
         >
@@ -56,7 +55,12 @@
     </el-dialog>
 
     <!-- 表格 -->
-    <el-table :data="records" border style="width: 100%; margin: 20px 0">
+    <el-table
+      v-loading="loading"
+      :data="records"
+      border
+      style="width: 100%; margin: 20px 0"
+    >
       <el-table-column type="index" label="序号" width="80" align="center">
       </el-table-column>
       <el-table-column prop="tmName" label="品牌名称"> </el-table-column>
@@ -66,59 +70,12 @@
         </template>
       </el-table-column>
       <el-table-column label="操作">
-        <template slot-scope="scope">
-          <el-button
-            type="warning"
-            icon="el-icon-edit"
-            @click="handleEdit(scope.$index, scope.row)"
+        <template slot-scope="{ row }">
+          <el-button type="warning" icon="el-icon-edit" @click="edit(row)"
             >修改</el-button
           >
-          <!-- 修改的弹出框 -->
-          <el-dialog
-            title="修改品牌"
-            :visible.sync="editDialogVisible"
-            width="50%"
-          >
-            <span>
-              <el-form
-                :model="editForm"
-                :rules="rules"
-                ref="editForm"
-                label-width="100px"
-              >
-                <el-form-item label="品牌名称" prop="tmName">
-                  <el-input v-model="editForm.tmName"></el-input>
-                </el-form-item>
-                <el-form-item label="品牌LOGO" prop="logoUrl">
-                  <el-upload
-                    class="avatar-uploader"
-                    :action="`${$BASE_API}/admin/product/fileUpload`"
-                    :show-file-list="false"
-                    :on-success="handleAvatarSuccess"
-                    :before-upload="beforeAvatarUpload"
-                  >
-                    <img
-                      v-if="editForm.logoUrl"
-                      :src="editForm.logoUrl"
-                      class="avatar"
-                    />
-                    <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-                  </el-upload>
-                  <p>只能上传jpg/png文件，且不超过50kb</p>
-                </el-form-item>
-              </el-form>
-            </span>
-            <span slot="footer">
-              <el-button @click="editDialogVisible = false">取 消</el-button>
-              <el-button type="primary" @click="submitEditForm('editForm')"
-                >确 定</el-button
-              >
-            </span>
-          </el-dialog>
-          <el-button
-            type="danger"
-            icon="el-icon-delete"
-            @click="handleDelete(scope.$index, scope.row)"
+
+          <el-button type="danger" icon="el-icon-delete" @click="del(row)"
             >删除</el-button
           >
         </template>
@@ -149,22 +106,23 @@ export default {
       page: 1, // 当前页码
       limit: 3, // 每页显示条数
       total: 0, // 总条数
-      addDialogVisible: false, // 增加的弹出框默认隐藏
-      editDialogVisible: false, // 修改的弹出框默认隐藏
+      visible: false, // 弹出框默认隐藏
+      loading: false, // 加载中的样式
       // 新增品牌收集数据
       trademarkForm: {
-        tmName: "",
-        logoUrl: "",
-      },
-      editForm: {
-        id: "",
         tmName: "",
         logoUrl: "",
       },
       // 新增品牌表单校验规则
       rules: {
         tmName: [
-          { required: true, message: "请输入品牌名称", trigger: "blur" },
+          {
+            required: true,
+            message: "请上传品牌logo",
+            // 自定义检验规则
+            // validator: this.validator,
+            trigger: "blur",
+          },
         ],
         logoUrl: [{ required: true, message: "请上传品牌logo" }],
       },
@@ -173,6 +131,7 @@ export default {
   methods: {
     // 获取品牌数据的方法
     async getTrademarkList(page, limit) {
+      this.loading = true;
       const result = await this.$API.trademark.getPageList(page, limit);
       if (result.code === 200) {
         this.$message.success("获取品牌数据成功");
@@ -182,7 +141,25 @@ export default {
         this.total = result.data.total;
       } else {
         this.$message.error("获取品牌数据失败");
+        this.loading = false;
       }
+      this.loading = false;
+    },
+    // 自定义表单校验的规则
+    validator(rule, value, callback) {
+      /**
+       * rule 校验的字段名
+       * value 校验的字段值
+       * callback 决定表单校验成功/失败，必须调用
+       */
+      if (!value) {
+        callback(new error("请输入品牌名称"));
+        return;
+      } else if (value.length < 2 || value.length > 10) {
+        callback(new Error("输入品牌名称的长度应为2-10位"));
+        return;
+      }
+      callback();
     },
     // 上传图片之前校验大小和格式的方法
     beforeAvatarUpload(file) {
@@ -202,60 +179,64 @@ export default {
     handleAvatarSuccess(file) {
       this.trademarkForm.logoUrl = file.data;
     },
-    // 增加品牌信息
+    // 点击添加按钮
+    add() {
+      // 弹出框显示、清空表单内容、清除校验规则
+      this.visible = true;
+      this.trademarkForm = { tmName: "", logoUrl: "" };
+      this.$refs.trademarkForm && this.$refs.trademarkForm.clearValidate();
+    },
+    // 点击修改按钮
+    edit(row) {
+      this.visible = true;
+      this.$refs.trademarkForm && this.$refs.trademarkForm.clearValidate();
+      // 将当前行的信息赋值给表单
+      this.trademarkForm = { ...row };
+    },
+    // 增加以及修改品牌信息
     submitForm(from) {
       // 校验表单
       this.$refs[from].validate(async (valid) => {
         // 表单验证通过valid为true，失败为false
         if (valid) {
-          // 发送增加品牌数据的请求
-          const result = await this.$API.trademark.addTrademark(
-            this.trademarkForm
-          );
-          if (result.code === 200) {
-            this.$message.success("添加品牌数据成功");
-            // 隐藏上传框
-            this.addDialogVisible = false;
-            // 重新加载品牌数据列表
-            this.getTrademarkList(this.page, this.limit);
+          const { trademarkForm } = this;
+          // 校验通过，判断是修改还是删除
+          const isUpdate = !!trademarkForm.id;
+          let result;
+          if (isUpdate) {
+            // 如果是修改要判断数据是否有变化
+            const record = this.records.find(
+              (record) => record.id === trademarkForm.id
+            );
+            if (
+              record.tmName === trademarkForm.tmName &&
+              record.logoUrl === trademarkForm.logoUrl
+            ) {
+              this.$message.warning("不能提交和之前一样的数据");
+              return;
+            }
+            // 如果有变化就发送修改的请求
+            result = await this.$API.trademark.updataTrademark(trademarkForm);
           } else {
-            this.$message.error("添加品牌数据失败");
+            // 如果是增加就发送增加品牌信息的请求
+            result = await this.$API.trademark.addTrademark(trademarkForm);
           }
-        }
-      });
-    },
-    // 修改品牌信息的弹框回调函数
-    handleEdit(index, row) {
-      this.editDialogVisible = true;
-      this.editForm.id = row.id;
-      this.editForm.logoUrl = row.logoUrl;
-    },
-    // 修改品牌信息
-    submitEditForm(from) {
-      // 校验表单
-      this.$refs[from].validate(async (valid) => {
-        // 表单验证通过valid为true，失败为false
-        if (valid) {
-          console.log(this.editForm);
-          // 发送增加品牌数据的请求
-          const result = await this.$API.trademark.updataTrademark(
-            this.editForm
-          );
-          console.log(result);
+
+          // 请求发送成功提示
           if (result.code === 200) {
-            this.$message.success("修改品牌数据成功");
+            this.$message.success(`${isUpdate ? "修改" : "添加"}品牌数据成功`);
             // 隐藏上传框
-            this.editDialogVisible = false;
+            this.visible = false;
             // 重新加载品牌数据列表
             this.getTrademarkList(this.page, this.limit);
           } else {
-            this.$message.error("修改品牌数据失败");
+            this.$message.error(`${isUpdate ? "修改" : "添加"}品牌数据失败`);
           }
         }
       });
     },
     // 删除品牌信息
-    handleDelete(index, row) {
+    del(row) {
       this.$confirm("你确定要删除品牌数据吗？", "提示", {
         confirmButtonText: "确定",
         cancelButtonText: "取消",
